30df7488696651a69e823a1cb15d29aa91e620ae
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-20 21:38:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
-def introduction(name, language)
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to progam in #{language}."
+end
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
 end
